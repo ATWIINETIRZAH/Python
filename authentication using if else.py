8d7095b733b8ef6819cdf451b5282e91e45b98a9
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-02-08 07:49:12 -0600

    Add files via upload

---
+++ authentication using if else.py
@@ -0,0 +1,9 @@
+username=input("enter yuour username:")
+password=int(input("enter your password: "))
+
+if username=="David" and password== 1234:
+  print("why hello",username,"what a lovely accent you have, you could have charmed your way in here even without a password")
+elif username=="Tirzah" and password== 3456:
+  print("why hello",username,"what a lovely accent you have, you could have charmed your way in here even without a password")
+else:
+  print("get outta here!")
---
+++ using if else statements.py
@@ -0,0 +1,24 @@
+
+
+print("Marvel movie Character Creator!!!")
+spider=input("do you like hanging around?")
+if spider == "yes":
+  print("then you are \033[31mspider-man\033[0m")
+else:
+  print("then you are not spider-man")
+
+print("")
+
+korg=input("do you have a 'gravely' voice?")
+if korg=="yes":
+  print("then you are \033[35m korg \033[0m")
+else:
+  print("aww, then you are not korg")
+
+print("")
+
+marvel=input("do you often 'marvelous'?")
+if marvel =="yes":
+  print("ahaa! you are \033[32mCaptain Marvel\033[0m Hi!")
+else:
+  print("you are not captain marvel")
