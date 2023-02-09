d6a7d6c921165be1b8472db54211be4376dfa12b
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-02-09 05:27:10 -0600

    Add files via upload

---
+++ daily motivation.py
@@ -0,0 +1,28 @@
+print("DAILY MOTIVATION!!")
+name=input("what is your name?: ")
+okay=input("are you having a good day or not?")
+
+if okay== "yes":
+  print("glad to hear that!",name)
+elif okay=="kinda":
+  print("it will get better xoxo")
+else:
+  print("i hope your day gets better")
+  
+day=input("what day is it??")
+if day=="Monday" or day=="monday" or day=="MONDAY":
+  print("everything is going to be okay",name,"we believe in you")
+elif day=="Tuesday" or day=="tuesday" or day=="TUESDAY":
+  print("The sky is not the limit!")
+elif day=="Wednesday" or day=="wednesday" or day=="WEDNESDAY":
+  print("you got this",name,"keep going!")
+elif day=="thursday" or day=="Thursday" or day=="THURSDAY":
+  print("dont let that rock stop you from getting there",name,"keep going!")
+elif day=="friday" or day=="Friday"or day=="FRIDAY":
+  print("keep pushing, We're proud of you",name)
+elif day=="saturday" or day=="Saturday" or day=="SATURDAY":
+  print("I know this won't be easy, but I also know you've got what it takes to get through it")
+elif day=="sunday" or day=="Sunday" or day=="SUNDAY":
+  print("Hang in there.Don't give up.")
+else:
+  print("haha. enjoy your day")
---
+++ nested if statements.py
@@ -0,0 +1,13 @@
+print("Fake Fan Finder:)")
+print("-+------------------------------+-")
+
+anime=input("whats your favorite Anime?: ")
+if anime=="Kuroko's basketball":
+  print("huh really??")
+  char=input("who's your favorite character?: ")
+  if char=="kuroko":
+    print("who else would it be right?")
+  else:
+    print("ohh shocker!")
+else:
+  print("interesting")
