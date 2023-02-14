1235cd320f98b9a0ec036225d04955df98820f6a
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-02-14 00:59:25 -0600

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,18 @@
+exit="no"
+while exit!="yes":
+  animal=input("what animal do you want?: ")
+  if animal=="cow":
+    print("a cow goes moo")
+    exit=input("do you want to exit?")
+  elif animal=="pig":
+    print("a pig goes oink")
+    exit=input("do you want to exit?")
+  elif animal=="duck":
+    print("a duck goes quack")
+    exit=input("do you want to exit?")
+  elif animal=="snake":
+    print("a snake goes hisssss")
+    exit=input("do you want to exit?")
+  else:
+    exit=input("do you want to exit?")
+  
