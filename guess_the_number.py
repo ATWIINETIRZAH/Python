5241de5e6593c47fa2f3c8ae6cfb97ab99cb7b17
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-02-16 14:25:43 -0600

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,21 @@
+print("enter number between 0 and 1,000,000:)")
+counter=0
+correct_number=678900
+while True:
+  num=int(input("what is your guess?"))
+  if num == correct_number:
+    print("Correct!")
+    counter+=1
+    break
+  elif num > correct_number:
+    print("Too high")
+    counter+=1
+    continue
+  elif num < correct_number and num>0:
+    print("too low")
+    counter+=1
+    continue
+  elif num<0:
+    exit()
+print("it took you",counter,"tries to get it right")
+  
