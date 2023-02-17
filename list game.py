b1d4ec8ccd6c83204165fa87fc558e577f13484d
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-02-17 00:20:22 -0600

    Add files via upload

---
+++ list game.py
@@ -0,0 +1,7 @@
+print("LIST GAME!!!")
+start=int(input("Enter start number: "))
+end=int(input("Enter stop number: "))
+incre=int(input("Increment between values: "))
+
+for i in range(start,end,incre):
+  print(i)
