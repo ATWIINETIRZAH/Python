51dfd51a5b36d9eb7789a3a876d3b831f608ab1d
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-03-02 14:57:45 -0600

    Add files via upload

---
+++ python basics.py
@@ -0,0 +1,14 @@
+print("hello world!")
+
+#use hashtags to comment
+
+name="Tirzah" #variable is a placeholder for certain data
+print(name)
+
+#prompt a user to input data using the input function
+password=int(input("enter your password: "))
+
+#different datatypes include int for integers, float for decimal numbers
+
+cgpa=float(input("enter your cgpa: "))
+
